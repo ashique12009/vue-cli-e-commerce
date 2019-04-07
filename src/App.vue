31976b491afab827b28d2b9be666455622393d17
ashique <ashique12009@gmail.com>
2019-04-07 21:02:34 +0600

    Navbar, Inventory and Cart templates are separated

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar></navbar>
+
+    <div class="container">
+      <div class="row custom_row">
+        <div class="col-sm-9">
+          <inventory></inventory>
+        </div>
+        <div class="col-sm-3">
+          <h4>Cart</h4>
+          <cart></cart>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Inventory from './components/Inventory';
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Navbar,
+    Cart,
+    Inventory
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.custom_row {
+   display: flex;
+   flex-wrap: wrap;
+}
+.custom_row > div[class*='col-'] {
+  display: flex;
 }
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="list-group">
+        <li class="list-group-item">
+            
+        </li>
+    </ul>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
